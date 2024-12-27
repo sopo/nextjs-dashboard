f8e42509a01685b5868a6579c745b9d99debcff4
sopo <sofiko.imnaishvili@gmail.com>
2024-12-27 18:47:31 +0200

    add login

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,7 @@
 'use server';
+ 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
@@ -110,4 +113,21 @@ export async function createInvoice(prevState: State, formData: FormData) {
       return { message: 'Database Error: Failed to Delete Invoice.' };
     }
   }
-
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
+  }
