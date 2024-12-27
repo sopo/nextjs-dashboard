8b2e8e5c7a488355b7cf791ac32af0a671a07410
sopo <sofiko.imnaishvili@gmail.com>
2024-12-27 11:41:40 +0200

    pnpm install

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,7 +13,6 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-//test
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
